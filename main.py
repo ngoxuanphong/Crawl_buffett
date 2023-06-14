ade0017dd2e08a31f85f713f9e7c8cec93c5b8a1
NgoXuanPhong <xuanphongcht@gmail.com>
2023-06-14 21:52:13 +0700

    add new data

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from save_pdf import GetPDF
 
-bf = GetPDF(path_all_com = 'docs/List_company_23052023 - Listing.csv',
-            path_save = 'Data')
-bf.get_all_com(reverse = True)
+bf = GetPDF(
+    path_all_com="docs/List_company_23052023 - Listing.csv",
+    path_save="Data",
+    time_sleep=35,
+)
+bf.get_all_com(reverse=True)
