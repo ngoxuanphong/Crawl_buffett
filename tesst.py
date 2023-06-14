35f8e98b028919b276997fa2876a91809ea7489a
ngoxuanphong <phong@cht.edu.vn>
2023-06-14 15:53:13 +0700

    add new download pdf

--- tesst.py
+++ tesst.py
@@ -1,18 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-time.sleep(3)
-elem.send_keys("pycon")
-time.sleep(3)
-elem.send_keys(Keys.RETURN)
-time.sleep(3)
-assert "No results found." not in driver.page_source
-time.sleep(3)
-driver.close()
+
+tor_proxy = "127.0.0.1:9150"
+
+chrome_options = Options()
+
+'''chrome_options.add_argument("--test-type")'''
+chrome_options.add_argument('--ignore-certificate-errors')
+'''chrome_options.add_argument('--disable-extensions')'''
+chrome_options.add_argument('disable-infobars')
+'''chrome_options.add_argument("--incognito")'''
+chrome_options.add_argument('--user-data=C:\\Users\\user\\AppData\\Local\\Google\\Chrome\\User Data\\Default')
+chrome_options.add_argument('--proxy-server=socks5://%s' % tor_proxy)
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://www.buffett-code.com/')
+time.sleep(4)
