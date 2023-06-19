662a3ae816664869228fba45e2ad6ac4bccf38e3
NgoXuanPhong <xuanphongcht@gmail.com>
2023-06-19 16:30:59 +0700

    update list file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from save_pdf import GetPDF
 bf = GetPDF(
             path_all_com="docs/List_company_23052023 - Listing.csv",
             path_save="Data",
-            time_sleep=30,
+            time_sleep=35,
             browser_name="Chrome",
             # tor_path=r"A:\Tor Browser",
             # headless=True
