eca75445b90685b653cf3096261fe3ae22f1a414
ngoxuanphong <phong@vis-laboratory.com>
2023-06-22 10:29:21 +0700

    update done data

--- read_pdf.py
+++ read_pdf.py
@@ -2,4 +2,4 @@ from src.readPdf import ReadPdf
 
 read_pdf = ReadPdf(path_all_com="docs/List_company_23052023 - Listing.csv",
                    path_save='Data')
-read_pdf.get_all_com(reverse=True)
+read_pdf.get_all_com(reverse=True)                                                                                                  
