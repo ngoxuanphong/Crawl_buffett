b08e2189c008ab6fa397342c116092c57e8a2244
ngoxuanphong <phong@cht.edu.vn>
2023-06-30 10:13:25 +0700

    add get proxy

--- tesst.py
+++ tesst.py
@@ -1,4 +1,12 @@
-from src.volume import GetVolume
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
-gv = GetVolume(path_save='')
-gv.getVolume(id_company=1418, return_df=True, save_file=False)
+url = 'https://www.proxynova.com/proxy-server-list/'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+tables = soup.find_all('table', id='tbl_proxy_list')
+
+df = pd.read_html(str(tables[0]))[0]
+print(df)
+print(len(tables))
