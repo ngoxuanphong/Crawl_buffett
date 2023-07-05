46c3f266bcb1eafc6c53851296007c9fb904f01c
ngoxuanphong <phong@vis-laboratory.com>
2023-07-06 04:45:37 +0700

    update data

--- src/savePDF.py
+++ src/savePDF.py
@@ -527,9 +527,9 @@ class GetPDF:
             list of symbol doing
         """
         df = pd.read_csv(self.path_all_com)
-        id = df[df["check"] != "Done"].index[1]
+        id = df[df["check"] == "Done"].index[1]
         if reverse:
-            id = df[df["check"] == 'True'].index[-1]
+            id = df[df["check"] == 'Done'].index[-1]
         symbol = df["Symbol"][id]
         df.loc[id, "check"] = "Doing"
         df.to_csv(self.path_all_com, index=False)
@@ -554,7 +554,7 @@ class GetPDF:
             id_company = self.getSymbolDoing(reverse=reverse)
             self.savePDF(id_company = id_company)
             self.savePDF(id_company = id_company)
-            msg = 'Done'
+            msg = 'True'
         except:
             msg = 'False'
 
--- tesst.py
+++ tesst.py
@@ -2,18 +2,18 @@ import threading as th
 from src.savePDF import GetProxyDriver, GetPDF
 import time 
 
-thread_num = 20
+thread_num = 7
 
 
 def run():
     bf = GetPDF(
         path_all_com="docs/List_company_23052023 - Listing.csv",
         path_save="Data",
-        time_sleep=20,
+        time_sleep=10,
         browser_name='Thread',
         # driver_temp= driver
     )
-    bf.savePDFThread(reverse=True)
+    bf.savePDFThread(reverse=False)
 
 if __name__ == "__main__":  # confirms that the code is under main function
     procs = []
