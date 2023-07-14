0364ac78dac73ce2f233e94b4039fa47651ddbc7
ngoxuanphong <phong@vis-laboratory.com>
2023-07-14 07:44:42 +0700

    hi

--- tesst.py
+++ tesst.py
@@ -15,7 +15,7 @@ def run():
         # driver_temp= driver
     )
     # bf.savePDFThread(reverse=True)
-    bf.multiThreadFile(reverse=True)
+    bf.multiThreadFile(reverse=False)
 
 if __name__ == "__main__":  # confirms that the code is under main function
     procs = []
