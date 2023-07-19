89d88fff500839dfa2da5312a867cb95775ef593
ngoxuanphong <phong@vis-laboratory.com>
2023-07-19 17:03:57 +0700

    update data

--- dividend.py
+++ dividend.py
@@ -1,8 +1,8 @@
 from src.readPdf import ReadPdf
 
-read_pdf = ReadPdf(path_all_com="docs/List_company_23052023 - Listing.csv",
+read_pdf = ReadPdf(path_all_com="docs\May_anhNA.csv",
                    path_save='')
-read_pdf.getAllCom(reverse=True, 
+read_pdf.getAllCom(reverse=False, 
                      bool_get_volume=False, 
                      bool_get_dividend=True, 
                      bool_get_table=False)
