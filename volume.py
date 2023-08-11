f99572c426f9cae2343d1db3d0eba6382eacf867
NgoXuanPhong <xuanphongcht@gmail.com>
2023-08-11 16:19:51 +0700

    update

--- volume.py
+++ volume.py
@@ -1,6 +1,7 @@
 from src.readPdf import ReadPdf
 
-read_pdf = ReadPdf(path_all_com="docs\May_anhNA.csv",
+# read_pdf = ReadPdf(path_all_com="docs\May_anhNA.csv",
+read_pdf = ReadPdf(path_all_com="docs\may_cty.csv",
                    path_save='')
 read_pdf.getAllCom(reverse=True, 
                      bool_get_volume=True, 
